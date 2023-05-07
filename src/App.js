549bf1626eeab2884f36d31e20be31d2af057b0d
nyo <nyo4rdace@gmail.com>
2023-05-07 21:23:30 +0800

    import timeline items from json file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-import Timeline from "./timeline.js"
+import React from "react";
+import { Chrono } from 'react-chrono';
+import "./App.css"
+import dd from "./data.json"
+
+const Timeline = () => {
+  return (
+    <div className="Timeline-box">
+      <Chrono
+        items={dd}
+        mode="VERTICAL"
+        slideShow
+        slideItemDuration={3000}
+        scrollable
+      />
+    </div>
+  );
+};
 
 function App() {
   return (
